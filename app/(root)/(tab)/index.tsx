3b4e428cf6970aa0586ea9214596ea177d81132e
Anhnguyenanh123 <138548969+Anhnguyenanh123@users.noreply.github.com>
2025-01-22 02:07:14 +0700

    config and create first look of the app

---
+++ app/(root)/(tab)/index.tsx
@@ -0,0 +1,20 @@
+import {Text, View} from "react-native";
+import {Link} from "expo-router";
+
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text className="font-bold my-10 font-rubik text-3xl">Welcome To ReState</Text>
+            <Link href="/sign-in">Sign in</Link>
+            <Link href="/explore">Explore</Link>
+            <Link href="/profile">Profile</Link>
+            <Link href="/properties/1">Property</Link>
+        </View>
+    );
+}
---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,23 @@
+import {useGlobalContext} from "@/lib/global-provider";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {ActivityIndicator} from "react-native";
+import {Redirect, Slot} from "expo-router";
+
+export default function AppLayout() {
+    const {loading, isLoggedIn} = useGlobalContext();
+
+    if (loading) {
+        return (
+            <SafeAreaView className="bg-white flex justify-center items-center h-full">
+                <ActivityIndicator
+                    className="text-primary-300"
+                    size="large"
+                />
+            </SafeAreaView>
+        )
+    }
+
+    if (!isLoggedIn) return <Redirect href="/sign-in" />
+
+    return <Slot/>
+}
